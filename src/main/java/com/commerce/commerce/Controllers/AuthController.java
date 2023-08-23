3a5bcb987ca600af08130f8f8b89588614af7515
peterKim <51915958+Peterkimliz@users.noreply.github.com>
2023-08-23 16:22:20 +0300

    Update AuthController.java

--- src/main/java/com/commerce/commerce/Controllers/AuthController.java
+++ src/main/java/com/commerce/commerce/Controllers/AuthController.java
@@ -3,7 +3,7 @@ package com.commerce.commerce.Controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +20,7 @@ public class AuthController {
 
 private final Authservice authservice;
 
- @GetMapping("/signup")
+ @PostMapping("/signup")
  public ResponseEntity<Authresponse> createUser(@RequestBody  @Validated  Register register){
     return new ResponseEntity<Authresponse>(authservice.createUser(register), HttpStatus.CREATED);
  }   
