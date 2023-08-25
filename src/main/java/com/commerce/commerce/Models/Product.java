29d2a6a746054f36b899dca66e06cf62df717c8c
PeterLojiKim <pkironji@yahoo.com>
2023-08-25 22:21:00 +0300

    created product model

--- src/main/java/com/commerce/commerce/Models/Product.java
+++ src/main/java/com/commerce/commerce/Models/Product.java
@@ -1,5 +1,27 @@
 package com.commerce.commerce.Models;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private String category;
+    private String subCategory;
+    private ArrayList<String> images;
+    private int price;
+    private int quantity;
+    private Date createdAt;
+    private String updatedAt;
     
 }
