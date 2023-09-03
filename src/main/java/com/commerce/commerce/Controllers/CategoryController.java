44e8f6817247462fd375cbe6ebc26a7deee98e3a
PeterLojiKim <pkironji@yahoo.com>
2023-09-03 19:43:11 +0300

    update annotations in the subcategory controller

--- src/main/java/com/commerce/commerce/Controllers/CategoryController.java
+++ src/main/java/com/commerce/commerce/Controllers/CategoryController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.commerce.commerce.Dtos.CategoryDto;
-import com.commerce.commerce.Dtos.SubcategoryDto;
 import com.commerce.commerce.Services.CategoryService;
 
 @RestController
--- src/main/java/com/commerce/commerce/Controllers/SubcategoryController.java
+++ src/main/java/com/commerce/commerce/Controllers/SubcategoryController.java
@@ -7,8 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,13 +33,13 @@ public class SubcategoryController {
                 HttpStatus.CREATED);
 
     }
-    @PostMapping("/create/{id}")
+    @PutMapping("/create/{id}")
     public ResponseEntity<SubcategoryDto> updateSubCategory(@RequestBody SubcategoryDto subcategoryDto,@PathVariable("id") String id) {
         return new ResponseEntity<SubcategoryDto>(subcategoryService.updateSubCategory(id,subcategoryDto),
                 HttpStatus.OK);
 
     }
-    @PostMapping("/all/{categoryId}")
+    @GetMapping("/all/{categoryId}")
     public ResponseEntity<List<SubcategoryDto>> getSubCategories(@PathVariable("categoryId") String categoryId) {
         return new ResponseEntity<List<SubcategoryDto>>(subcategoryService.findSubcategoriesWithCategoryId(categoryId),HttpStatus.OK);
 
